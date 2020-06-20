5304c76840b29ca85369f9e94d49e67835ac9174
Mykola Makhin <mvmn.inbox@gmail.com>
2020-06-20 12:04:03 +0300

    Conditionally download map SVG from Wikipedia - only do this if file doesn't exist yet.

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
-wget https://upload.wikimedia.org/wikipedia/uk/0/00/Map_of_Ukraine_Oblasts_DEMO.svg
+if [ ! -f Map_of_Ukraine_Oblasts_DEMO.svg ]; then
+  wget https://upload.wikimedia.org/wikipedia/uk/0/00/Map_of_Ukraine_Oblasts_DEMO.svg
+fi
 
